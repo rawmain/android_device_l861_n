5d39a55bd37e7a05168f3fcbeb57f97ff91f6d87
RAW MAIN <rawmain@gmail.com>
2017-12-07 18:21:10 +0100

    Update AndroidBoard.mk

--- AndroidBoard.mk
+++ AndroidBoard.mk
@@ -5,4 +5,4 @@ include $(CLEAR_VARS)
 ALL_PREBUILT += $(INSTALLED_KERNEL_TARGET)
 
 # include the non-open-source counterpart to this file
--include vendor/leeco/x3/AndroidBoardVendor.mk
+-include vendor/amoi/l861/AndroidBoardVendor.mk
